2187f1f52b37756ad773cc9a77b3974d8f8e7f4a
DaylightExSrc <littlecanionero3@gmail.com>
2026-03-24 04:59:56 -0400

    Update leaked by discord.ggsabscripts.lua

--- leaked by discord.ggsabscripts.lua
+++ leaked by discord.ggsabscripts.lua
@@ -1669,7 +1669,7 @@ task.spawn(function()
     titleLabel.Size = IS_MOBILE and UDim2.new(0.4, 0, 1, 0) or UDim2.new(0.6, 0, 1, 0)
     titleLabel.Position = UDim2.new(0, 14, 0, 0)
     titleLabel.BackgroundTransparency = 1
-    titleLabel.Text = "STEAL HELPER"
+    titleLabel.Text = "Lethals - Auto Steal"
     titleLabel.Font = Enum.Font.GothamBlack
     titleLabel.TextSize = 15
     titleLabel.TextColor3 = Color3.fromRGB(200, 180, 255)
